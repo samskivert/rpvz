6726c90ad55f521dfe1ade14759bf2e21ef4dfe2
Michael Bayne <mdb@samskivert.com>
2019-07-26 14:19:20 -0700

    Starting on unit config, better collisions.

--- src/game.ts
+++ src/game.ts
@@ -36,15 +36,52 @@ function makeUnitTrans (tile :Tile, gx :number, gy :number) {
   return makeTransform(tile.size[0]/2, tile.size[1], cx, cy, 1, 1, 0)
 }
 
-function makeUnit (tile :Tile, vx :number, gx :number, gy :number) {
+type UnitConfig = {
+  tile :string,
+  health :number,
+  speed? :number,
+}
+
+const plants = {
+  shooter: {
+    tile: "shooter",
+    health: 100,
+  },
+  threepeater: {
+    tile: "threepeater",
+    health: 100,
+  }
+}
+
+const zombs = {
+  normal: {
+    tile: "suit",
+    health: 100,
+    speed: -20,
+  },
+  glitter: {
+    tile: "glitter",
+    health: 50,
+    speed: -30,
+  },
+}
+
+function makeUnit (cfg :UnitConfig, tile :Tile, gx :number, gy :number) {
   return {
     components: {
       trans: {initial: makeUnitTrans(tile, gx, gy)},
       tile: {initial: tile},
-      vel: {initial: vec2.fromValues(vx, 0)},
+      vel: {},
+      basevel: {initial: vec2.fromValues(cfg.speed || 0, 0)},
       lane: {initial: gy},
     }
   }
+}
+function makePlant (cfg :UnitConfig, texs :Textures, gx :number, gy :number) {
+  return makeUnit(cfg, texs.plants[cfg.tile], gx, gy)
+}
+function makeZomb (cfg :UnitConfig, texs :Textures, gx :number, gy :number) {
+  return makeUnit(cfg, texs.zombs[cfg.tile], gx, gy)
 }
 
 export class ShambleSystem extends System {
@@ -70,8 +107,9 @@ export class LaneCollideSystem extends System {
   constructor (domain :Domain,
                readonly trans :TransformComponent,
                readonly vel :Vec2Component,
+               readonly basevel :Vec2Component,
                readonly lane :Float32Component) {
-    super(domain, Matcher.hasAllC(trans.id, vel.id, lane.id))
+    super(domain, Matcher.hasAllC(trans.id, vel.id, basevel.id, lane.id))
     for (let ll = 0; ll < Lanes; ll += 1) this.laneIds[ll] = [] // sigh javascript
   }
 
@@ -80,23 +118,32 @@ export class LaneCollideSystem extends System {
       const ids = this.laneIds[lane]
       // x squared ftw! x is less than ten so whatever
       for (const id of ids) {
-        // if this entity is not moving, skip it
-        const tv = this.vel.read(id, tvel)
-        if (tv[0] === 0) continue
+        // if this entity does not move, skip it
+        const tbvx = this.basevel.read(id, tvel)[0]
+        if (tbvx === 0) continue
 
+        const tvx = this.vel.read(id, tvel)[0]
         const tx = this.trans.readTx(id)
         const twid = 150 // temp
         const tl = tx-twid/2, tr = tx+twid/2
 
+        let collided = false
         for (const cid of ids) {
           if (id === cid) continue
           const ctx = this.trans.readTx(cid)
           const ctwid = 150 // temp
           const ctl = ctx-ctwid/2, ctr = ctx+ctwid/2
           if (ctl < tl && tl < ctr) {
-            console.log(`E${id} collided with E${cid}`)
-            this.vel.update(id, vec2.set(tv, 0, tv[1]))
+            if (tvx !== 0) {
+              console.log(`E${id} collided with E${cid}`)
+              this.vel.update(id, vec2.set(tvel, 0, 0))
+            }
+            collided = true
           }
+        }
+        if (!collided && tvx === 0) {
+          console.log(`E${id} starting again`)
+          this.vel.update(id, this.basevel.read(id, tvel))
         }
       }
     }
@@ -132,49 +179,34 @@ export class GameMode extends SurfaceMode {
     const trans = new TransformComponent("trans", batchBits)
     const tile = new DenseValueComponent<Tile>("tile", texs.plants.pea)
     const vel = new Vec2Component("vel", vec2zero, batchBits)
-    const lane = new Float32Component("vel", 0, batchBits)
+    const basevel = new Vec2Component("basevel", vec2zero, batchBits)
+    const lane = new Float32Component("lane", 0, batchBits)
+    const health = new Float32Component("health", 0, batchBits)
 
-    const domain = this.domain = new Domain({}, {trans, tile, vel, lane})
-    this.lanecolsys = new LaneCollideSystem(domain, trans, vel, lane)
+    const domain = this.domain = new Domain({}, {trans, tile, vel, basevel, lane, health})
+    this.lanecolsys = new LaneCollideSystem(domain, trans, vel, basevel, lane)
     this.dynamsys = new DynamicsSystem(domain, trans, vel)
     // TODO: should we have the render system handle HiDPI scale?
     this.rendersys = new RenderSystem(domain, trans, tile)
 
     let gx = 0, gy = 0
-    domain.add(makeUnit(this.texs.plants.bonkchoy, 0, gx, gy++))
-    domain.add(makeUnit(this.texs.plants.shooter, 0, gx, gy++))
-    domain.add(makeUnit(this.texs.plants.chomper, 0, gx, gy++))
-    domain.add(makeUnit(this.texs.plants.five, 0, gx, gy++))
-    domain.add(makeUnit(this.texs.plants.gatling, 0, gx, gy++))
-    gy = 0; gx += 1;
-    domain.add(makeUnit(this.texs.plants.icequeen, 0, gx, gy++))
-    domain.add(makeUnit(this.texs.plants.snapdragon, 0, gx, gy++))
-    domain.add(makeUnit(this.texs.plants.threepeater, 0, gx, gy++))
-    domain.add(makeUnit(this.texs.plants.sunflower, 0, gx, gy++))
+    domain.add(makePlant(plants.shooter, texs, gx, gy++))
+    domain.add(makePlant(plants.threepeater, texs, gx, gy++))
+    domain.add(makePlant(plants.shooter, texs, gx, gy++))
+    domain.add(makePlant(plants.shooter, texs, gx, gy++))
+    domain.add(makePlant(plants.threepeater, texs, gx, gy++))
 
-    const zombv = -30
     gy = 0; gx = 8;
-    domain.add(makeUnit(this.texs.zombs.adventurer, zombv, gx, gy++))
-    domain.add(makeUnit(this.texs.zombs.advskull, zombv, gx, gy++))
-    domain.add(makeUnit(this.texs.zombs.cowboy, zombv, gx, gy++))
-    domain.add(makeUnit(this.texs.zombs.flag, zombv, gx, gy++))
-    domain.add(makeUnit(this.texs.zombs.glitter, zombv, gx, gy++))
-    gy = 0; gx -= 1;
-    domain.add(makeUnit(this.texs.zombs.pirate, zombv, gx, gy++))
-    domain.add(makeUnit(this.texs.zombs.jestercake, zombv, gx, gy++))
-    domain.add(makeUnit(this.texs.zombs.jetpack, zombv, gx, gy++))
-    domain.add(makeUnit(this.texs.zombs.onbird, zombv, gx, gy++))
-    domain.add(makeUnit(this.texs.zombs.parka, zombv, gx, gy++))
-    gy = 0; gx -= 1;
-    domain.add(makeUnit(this.texs.zombs.suit, zombv, gx, gy++))
-    domain.add(makeUnit(this.texs.zombs.twilight, zombv, gx, gy++))
+    domain.add(makeZomb(zombs.normal, texs, gx, gy++))
+    domain.add(makeZomb(zombs.normal, texs, gx, gy++))
+    domain.add(makeZomb(zombs.glitter, texs, gx, gy++))
+    domain.add(makeZomb(zombs.normal, texs, gx, gy++))
+    domain.add(makeZomb(zombs.glitter, texs, gx, gy++))
   }
 
   renderTo (clock :Clock, surf :Surface) {
-    // rotsys.update(clock)
     this.lanecolsys.update()
     this.dynamsys.update(clock)
-    // bouncesys.update()
     this.rendersys.update()
 
     surf.saveTx()
@@ -185,11 +217,5 @@ export class GameMode extends SurfaceMode {
     surf.restoreTx()
 
     this.rendersys.render(this.batch)
-
-    // for (let gy = 0; gy < gridH; gy += 1) {
-    //   for (let gx = 0; gx < gridW; gx += 1) {
-    //     drawUnit(surf, gx, gy, this.texs.plants.shooter)
-    //   }
-    // }
   }
 }
